002533fc3cf972ca233b422e2408edf880041d09
ala decoste <alshareefialaa@gmail.com>
2021-03-18 05:10:33 -0400

    Added server.js.

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -14,21 +10,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactrecipes",
-  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-);
+app.use(routes);
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
