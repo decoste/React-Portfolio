7a668d718449d662da49167f04c7c7831ab131a0
ala decoste <alshareefialaa@gmail.com>
2021-03-18 04:53:22 -0400

    Cleaned up the files, and removed unnecessary files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Education from './components/Education';
+import Certifications from './components/Certifications';
+import Portfolio from './components/Portfolio';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
 
   return (
 <div>
-<Nav/>
+
    <Router>
+     
    <div className="App">
+   <Nav/>
      <Route exact path="/" component={Home} />
+     <Route exact path="/contact" component={Contact} />
+     <Route exact path="/education" component={Education} />
+     <Route exact path="/certifications" component={Certifications} />
+     <Route exact path="/portfolio" component={Portfolio} />
+     <Footer />
      </div>
+     
    </Router>
    </div>
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,84 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
+import Doc from '../../documents/resume.pdf'
 
 function Nav() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-       
-      </a>
-    </nav>
+   <nav className="navbar navbar-inverse navbar-expand-lg navbar-light  NavColor font fixed-top ">
+       <div>
+        <h3>Ala DeCoste</h3>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+     
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item ">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Contact"
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/portfolio"
+              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+            >
+              Portfolio
+            </Link>
+          </li>
+
+         
+          <li className="nav-item">
+            <Link
+              to="/certifications"
+              className={window.location.pathname === "/certifications" ? "nav-link active" : "nav-link"}
+            >
+              Certifications
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/education"
+              className={window.location.pathname === "/education" ? "nav-link active" : "nav-link"}
+            >
+              Education
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link   href={Doc} target="blank"
+              to={Doc} 
+              className={window.location.pathname === "/resume" ? "nav-link active" : "nav-link"}
+            >
+                
+                
+                
+              Resume
+            </Link>
+          </li>
+        </ul>
+        </div>
+        </div>
+     
+    </nav>       
   );
 }
 
