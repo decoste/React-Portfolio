1595f2c56ab96424dfa2154f4815ea23b0bee676
ala decoste <alshareefialaa@gmail.com>
2021-03-18 05:32:21 -0400

    Updated Package.json to deploy to Github

---
+++ client/build/precache-manifest.9af5a673eb9703d74d4b6bf64146000d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e592a8d25e407ebdb84116d0ad8ea964",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9c025a570d6b9da18abd",
+    "url": "/React-Portfolio/static/css/2.33c8dac8.chunk.css"
+  },
+  {
+    "revision": "b94b419f1857379eb4b2",
+    "url": "/React-Portfolio/static/css/main.72cb69b2.chunk.css"
+  },
+  {
+    "revision": "9c025a570d6b9da18abd",
+    "url": "/React-Portfolio/static/js/2.295a3aa7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.295a3aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b85c013fe0ab9d5524e",
+    "url": "/React-Portfolio/static/js/3.e7005ff4.chunk.js"
+  },
+  {
+    "revision": "b94b419f1857379eb4b2",
+    "url": "/React-Portfolio/static/js/main.02319de9.chunk.js"
+  },
+  {
+    "revision": "bec2bb03da078b5be895",
+    "url": "/React-Portfolio/static/js/runtime-main.fbd5c0a8.js"
+  },
+  {
+    "revision": "a47520f488cfa461e8e950d86972fd6e",
+    "url": "/React-Portfolio/static/media/JWAJ.a47520f4.gif"
+  },
+  {
+    "revision": "113f6c794ccc32304e3ff4296390c001",
+    "url": "/React-Portfolio/static/media/background.113f6c79.png"
+  },
+  {
+    "revision": "bc7d5b990d932f8da7bfeaf247290a44",
+    "url": "/React-Portfolio/static/media/budget.bc7d5b99.gif"
+  },
+  {
+    "revision": "b62e85ff274aa65d52edac2782e7e537",
+    "url": "/React-Portfolio/static/media/employee-dir.b62e85ff.gif"
+  },
+  {
+    "revision": "3b8cdf855638fda3c50bb19373b92e8b",
+    "url": "/React-Portfolio/static/media/fitness.3b8cdf85.gif"
+  },
+  {
+    "revision": "4a3945149dafdadb6efea8dcc535e9dc",
+    "url": "/React-Portfolio/static/media/gradu.4a394514.png"
+  },
+  {
+    "revision": "a4c3f65dcb6cbc641bf8df12e3f29463",
+    "url": "/React-Portfolio/static/media/me.a4c3f65d.jpg"
+  },
+  {
+    "revision": "44ce06c4260ecab9fa6fda2f0fd73538",
+    "url": "/React-Portfolio/static/media/note.44ce06c4.gif"
+  },
+  {
+    "revision": "66e71ed8adbb43f3e44b7565c9182220",
+    "url": "/React-Portfolio/static/media/passwordGenerator.66e71ed8.gif"
+  },
+  {
+    "revision": "0786b675a64252015c209d070df55b57",
+    "url": "/React-Portfolio/static/media/resume.0786b675.pdf"
+  },
+  {
+    "revision": "9e4e0afac832d827e00d0d54ee56402d",
+    "url": "/React-Portfolio/static/media/task.9e4e0afa.gif"
+  },
+  {
+    "revision": "b3b75becbb5b140696ccc6d1e67f4804",
+    "url": "/React-Portfolio/static/media/team.b3b75bec.gif"
+  },
+  {
+    "revision": "fe25610b328ec55be8dc437e8302991c",
+    "url": "/React-Portfolio/static/media/timeCapsule.fe25610b.gif"
+  },
+  {
+    "revision": "55359819b5208200d2c2d1d3f38076e2",
+    "url": "/React-Portfolio/static/media/weather.55359819.gif"
+  }
+]);
